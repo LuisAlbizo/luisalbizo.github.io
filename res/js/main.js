fc5a9f503fab10f7258ad54dcb3d63ce943f5ce0
LuisAlbizo <albizo.luis@gmail.com>
2018-09-16 00:22:55 -0500

    agregada animacion en nav-bar y cambiados titulos

--- res/js/main.js
+++ res/js/main.js
@@ -2,11 +2,13 @@
 var nav = new Vue({
 	el: "#nav",
 	data: {
-		display: true
+		display: true,
+		animationClass: "change"
 	},
 	methods: {
 		displayNav: function(event) {
 			this.display = !this.display;
+			this.animationClass = (this.animationClass == "change" ? "" : "change");
 		}
 	}
 });
